8b786418d178d0fc7004b907448f8618ce5cd563
Andras Toth <andras.toth@banzaicloud.com>
2018-10-18 14:14:57 +0200

    feat(helper): export helper module

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { default as config } from './config'
 export { default as logger } from './logger'
 export { default as catchErrors } from './catch-errors'
 export { default as gracefulShutdown } from './graceful-shutdown'
+import * as helper from './helper'
 import * as middleware from './middleware'
-export { middleware }
+export { helper, middleware }
